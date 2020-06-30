f4952900de5d00bc62ca57d69d3ac2820e8c0334
Gordei <gordei.ku.ro.ki@gmail.com>
2020-06-30 22:33:54 +1200

    Add Neuron class to solution

---
+++ Neuron.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmollBrein
+{
+    class Neuron
+    {
+        private bool state = false;
+        public Neuron[] InputConnections;
+        public Neuron[] OutputConnections;
+        public void SetConnections(Neuron[] InputConnections, Neuron[] OutputConnections)
+        {
+            this.InputConnections = InputConnections;
+            this.OutputConnections = OutputConnections;
+        }
+        public void CheckInputs()
+        {
+            foreach (Neuron neuron in InputConnections)
+            {
+                if (neuron.state)
+                {
+                    if (InputConnections[InputConnections.Length - 1] == neuron)
+                    {
+                        SetState(true);
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        public void SetState(bool State)
+        {
+            this.state = State;
+            if (State)
+            {
+                foreach (Neuron neuron in OutputConnections)
+                {
+                    neuron.CheckInputs();
+                }
+            }
+        }
+    }
+}
