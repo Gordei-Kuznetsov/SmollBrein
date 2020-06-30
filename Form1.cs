ca94110c95380df53b01702e1e73cd6209e289d7
Benicio Van Zwijndregt <benicio.vanzwijndregt@outlook.com>
2020-06-30 22:24:21 +1200

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//this was an edit i did from Visual studio code via github desktop, i just wanna see if it directly modifies the code or makes
+//another branch
+
 namespace SmollBrein
 {
     public partial class Form1 : Form
